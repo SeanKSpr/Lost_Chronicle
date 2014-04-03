b694d8c606d8a9e78d3b782cf87209db5cacd37c
SeanKSpr <zombiesoulcaliber@gmail.com>
2014-04-03 16:48:03 -0500

    Added Log message
    Added a Log message to the onCreate method of SelectionScreen.java.

--- Lost_Chronicle/src/edu/auburn/eng/csse/COMP3710/Group_22/lost_chronicle/SelectionScreen.java
+++ Lost_Chronicle/src/edu/auburn/eng/csse/COMP3710/Group_22/lost_chronicle/SelectionScreen.java
@@ -3,13 +3,14 @@ package edu.auburn.eng.csse.COMP3710.Group_22.lost_chronicle;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-
+import android.util.Log;
 public class SelectionScreen extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_selection_screen);
+      Log.i("TAG", "Git hub testing part dos");
 	}
 
 	@Override
