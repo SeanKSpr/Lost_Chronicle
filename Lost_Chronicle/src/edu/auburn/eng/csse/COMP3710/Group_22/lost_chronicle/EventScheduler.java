a272b70926f6014d9c71d4d418ec194680229d42
Sean <slw0023@auburn.edu>
2014-04-23 23:32:40 -0500

    Fixed some syntax

--- Lost_Chronicle/src/edu/auburn/eng/csse/COMP3710/Group_22/lost_chronicle/EventScheduler.java
+++ Lost_Chronicle/src/edu/auburn/eng/csse/COMP3710/Group_22/lost_chronicle/EventScheduler.java
@@ -78,8 +78,8 @@ public class EventScheduler {
 		event.setEnd_time(parseStringToDate(cursor.getString(3)));
 		event.setType(cursor.getString(4));
 		event.setOnGoing((short) Integer.parseInt(cursor.getString(5)));
-		event.setDifficulty((short) Integer.parseInt(cursor.getString(6)));
-		event.setEval((short) Integer.parseInt(cursor.getString(7)));
+		event.setDifficulty(Float.parseFloat(cursor.getString(6)));
+		event.setEval(Float.parseFloat(cursor.getString(7)));
 		event.setDescription(cursor.getString(8));
 		
 		//logcat it
